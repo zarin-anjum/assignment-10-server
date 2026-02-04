f6c9d9cd77b9fcee3f5073a21c1c55e2a430c307
zarin-anjum <zarinanjum0801@gmail.com>
2026-02-04 21:07:52 +0600

    Made final changes

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ import interestRoutes from "./routes/interestRoutes.js"
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/crops", cropRoutes);
 app.use("/api/interests", interestRoutes);
 
-const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+  );
+}
